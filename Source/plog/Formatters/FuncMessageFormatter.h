0258c4e45c90ebc2cf35f66905081aa0a1cc8606
Javinator9889 <javialonso007@hotmail.es>
2017-12-19 12:00:49 +0100

    Moved files to "Source"
    Now the code is in "Source" folder

---
+++ Source/plog/Formatters/FuncMessageFormatter.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iomanip>
+#include "../Util.h"
+#include "../Record.h"
+
+namespace plog
+{
+    class FuncMessageFormatter
+    {
+    public:
+        static util::nstring header()
+        {
+            return util::nstring();
+        }
+
+        static util::nstring format(const Record& record)
+        {
+            util::nstringstream ss;
+            ss << record.getFunc().c_str() << "@" << record.getLine() << ": ";
+            ss << record.getMessage().c_str() << "\n";
+
+            return ss.str();
+        }
+    };
+}
